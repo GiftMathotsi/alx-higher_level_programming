0a194dd5d9af31a7fafffe4b1c60559beffe8df1
Gift Mathotsi <giftnyiko234@gmail.com>
2024-01-15 18:45:15 +0000

    0-isupper.c

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - func check if uppercase letters
+ * @c: char to check
+ * Return: if uppercase return1, if not return 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
+}
